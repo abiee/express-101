2029032e6de47079eca1f49595ebf8f606479d96
Abiee Echamea <abiee.alejandro@gmail.com>
2016-07-28 15:11:09 -0500

    Include express middlewares

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var cors = require('cors');
+var morgan = require('morgan');
+var serveStatic = require('serve-static');
 var http = require('http');
 
 var app = express();
+
+// Log requests
+if (process.env.NODE_ENV == 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
+
+// Parse to json requests with application/json
+app.use(bodyParser.json())
+
+// Compress all responses
+app.use(compression());
+
+// Enable CORS for any origin
+app.use(cors())
+
+// Serve public assets
+app.use(serveStatic('app', { index: ['index.html', 'index.htm'] }));
 
 app.get('/hello/:name', (req, res) => {
   const name = req.params.name;
