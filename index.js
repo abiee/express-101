106dab344449784fff2e0213aab6b91f7d4785a1
Abiee Echamea <abiee.alejandro@gmail.com>
2016-07-28 12:08:54 -0500

    Paremetrize hello route

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var express = require('express')
-var http = require('http')
+var express = require('express');
+var http = require('http');
 
 var app = express();
 
-app.get('/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.json({ hello: name });
 });
 
 http.createServer(app).listen(3000, () => {
